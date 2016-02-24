24a0c82f9d4e26f3df94b72d71acf394c3dc9e2a
TheBlackfish <sniprpenguin@gmail.com>
2016-02-24 13:47:25 -0600

    localServerConstants.php should not be a part of the repo.

--- php/localServerConstants.php
+++
@@ -1,6 +0,0 @@
-<?php
-	
-	$phpFilepath = "C:\\xampp\\php\\php.exe";
-	$phpSchedulerLogPath = "C:\\xampp\\htdocs\\multisweeper.log";
-	$scriptsDirectory = "C:\\xampp\\htdocs\\minesweeper\\php\\";
-?>
